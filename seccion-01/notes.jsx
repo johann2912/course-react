931154796597fcbe5c0ceb2e45a1d0bd52078a39
Johann Gomez C <johanngomez02@gmail.com>
2022-04-08 13:52:31 -0500

    [CREATE] first steps with react

---
+++ seccion-01/notes.jsx
@@ -0,0 +1,13 @@
+/*
+    Temas de esta primera sección
+    1 - Que react
+        - Es una libreria, esta hecho para trabajar con aplicaciones pequeñas, medianas y grandes.
+          react cuando renderiza lo hace sobre un elemento especifico, react es de una sola via; asi
+          evitando mutaciones y trabaja con componentes. Se pueden crear aplicaciones moviles haciendo
+          uso de react native y para aplicaciones de escritorio se puede utilizar Electron.
+
+    2 - Conceptos generales
+        -
+    3 - Babel
+    4 - JSX
+*/ 
